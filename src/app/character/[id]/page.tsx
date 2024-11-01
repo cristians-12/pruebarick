f93536386b758add5b4b1baab258e251822054bb
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-11-01 17:25:24 -0500

    detallando personaje

--- src/app/character/[id]/page.tsx
+++ src/app/character/[id]/page.tsx
@@ -1,10 +1,14 @@
-// pages/character/[id].tsx
+// app/character/[id]/page.tsx
 import React from 'react';
 import { Character } from '../../../../types/api/characters';
 import Image from 'next/image';
+import { FaCircle } from 'react-icons/fa';
 
-const CharacterDetail = async ({ params }: { params: { id: string } }) => {
+interface CharacterDetailProps {
+    params: { id: string };
+}
 
+const CharacterDetail = async ({ params }: CharacterDetailProps) => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_CHARACTERS_URL}/${params.id}`, {
         cache: 'no-store',
     });
@@ -16,12 +20,26 @@ const CharacterDetail = async ({ params }: { params: { id: string } }) => {
     const character: Character = await res.json();
 
     return (
-        <div>
-            <h1>{character.name}</h1>
-            <Image width={300} height={300} src={character.image} alt={character.name} />
-            <p>Species: {character.species}</p>
-            <p>Status: {character.status}</p>
-
+        <div className='px-5 flex'>
+            <div className='w-[50%]'>
+                <figure className='w-[50%]'>
+                    <Image className='w-full' src={character.image} width={500} height={500} alt={character.name} />
+                </figure>
+                <h1 className='text-[30px] font-bold'>{character.name}</h1>
+                <div className='flex items-center gap-3'>
+                    <h2>Estado: {character.status}</h2>
+                    {character.status == 'Dead' ? <FaCircle fill='red' /> : <FaCircle fill='green' />}
+                </div>
+                <h3>Especie: {character.species}</h3>
+                <p>
+                    Origen: {character.origin.name}
+                </p>
+                <p>Genero: {character.gender}</p>
+            </div>
+            <div>
+                <p className='text-[30px]'>Ubicado en: {character.location.name}</p>
+                <p className='text-[30px]'>Viene de: {character.origin.name}</p>
+            </div>
         </div>
     );
 };
