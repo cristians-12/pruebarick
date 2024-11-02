adac32d3c7aafa5204ea1aa27dd189b48094e32b
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-11-02 18:10:28 -0500

    button visibility

--- src/components/ButtonDatabase.tsx
+++ src/components/ButtonDatabase.tsx
@@ -8,7 +8,7 @@ const ButtonDatabase: React.FC = () => {
   return (
     <div
       onClick={addDocument}
-      className="fixed lg:bottom-5 bottom-0 lg:w-auto w-[100px] bg-green-400 text-black px-3 py-2 rounded-xl cursor-pointer hover:scale-105 lg:right-5"
+      className="fixed lg:bottom-5 bottom-0 lg:w-auto w-[100px] bg-green-700 bg-opacity-70 text-white px-3 py-2 rounded-xl cursor-pointer hover:scale-105 lg:right-5 hover:bg-green-600 transition-all duration-200 ease-in-out shadow-lg"
     >
       <span>Guardar mis favoritos</span>
     </div>
