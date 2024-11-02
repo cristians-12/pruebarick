e1def9bee0833703e292c430e7ede60504b9a42a
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-11-02 14:28:51 -0500

    guardar btn

--- src/components/ButtonDatabase.tsx
+++ src/components/ButtonDatabase.tsx
@@ -8,7 +8,7 @@ const ButtonDatabase: React.FC = () => {
   return (
     <div
       onClick={addDocument}
-      className="fixed bottom-5 bg-green-400 text-black px-3 py-2 rounded-xl cursor-pointer hover:scale-105 right-5"
+      className="fixed lg:bottom-5 bottom-0 lg:w-auto w-[100px] bg-green-400 text-black px-3 py-2 rounded-xl cursor-pointer hover:scale-105 lg:right-5"
     >
       <span>Guardar mis favoritos</span>
     </div>
