203d72840add953555ca39db9b16d282932545e8
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-11-02 13:27:48 -0500

    analytics not used

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +13,9 @@ const firebaseConfig = {
   storageBucket: "rickymorty-d5a3e.firebasestorage.app",
   messagingSenderId: "128391058935",
   appId: "1:128391058935:web:61080d2bf11a136639caf2",
-  measurementId: "G-9NMN7EGYXJ"
+  measurementId: "G-9NMN7EGYXJ",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
