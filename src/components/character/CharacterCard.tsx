32667b281064cf21ecc923bb471fc46708078f92
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-11-02 13:48:46 -0500

    no use iamgee

--- src/components/character/CharacterCard.tsx
+++ src/components/character/CharacterCard.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React from "react";
 import { Character } from "../../../types/api/characters";
 import { motion } from "framer-motion";
@@ -8,58 +8,58 @@ import Link from "next/link";
 import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import {
-    addFavorite,
-    removeFavorite,
+  addFavorite,
+  removeFavorite,
 } from "../../../redux/features/favoritesSlice";
 
 const CharacterCard: React.FC<{ character: Character }> = ({ character }) => {
-    const favorites = useAppSelector(
-        (favorite) => favorite.favoritesReducer.value
-    );
-    const dispatch = useAppDispatch();
+  const favorites = useAppSelector(
+    (favorite) => favorite.favoritesReducer.value
+  );
+  const dispatch = useAppDispatch();
 
-    const isFavorite = favorites.some((element) => element.id === character.id);
+  const isFavorite = favorites.some((element) => element.id === character.id);
 
-    return (
-        <motion.div
-            whileHover={{ scale: 1.05 }}
-            transition={{ type: "spring", stiffness: 300 }}
-            className=" cursor-pointer flex flex-col lg:w-[20%] w-[44%] items-center justify-center gap-3"
-            variants={item}
-            key={character.id}
-        >
-            <Link
-                href={`/character/${character.id}`}
-                className="w-full flex flex-col gap-1"
-            >
-                <Image
-                    src={character.image}
-                    width={300}
-                    className="w-full"
-                    height={100}
-                    alt={character.name}
-                />
-                <span className="font-bold w-full">{character.name}</span>
+  return (
+    <motion.div
+      whileHover={{ scale: 1.05 }}
+      transition={{ type: "spring", stiffness: 300 }}
+      className=" cursor-pointer flex flex-col lg:w-[20%] w-[44%] items-center justify-center gap-3"
+      variants={item}
+      key={character.id}
+    >
+      <Link
+        href={`/character/${character.id}`}
+        className="w-full flex flex-col gap-1"
+      >
+        <img
+          src={character.image}
+          width={300}
+          className="w-full"
+          height={100}
+          alt={character.name}
+        />
+        <span className="font-bold w-full">{character.name}</span>
 
-                <span className="font-bold w-full">{character.species}</span>
-            </Link>
-            <div className="flex w-full justify-between items-center">
-                <span className="font-bold">{character.location.name}</span>
-                {isFavorite ? (
-                    <FaHeart
-                        fill="red"
-                        onClick={() => dispatch(removeFavorite(character))}
-                        size={28}
-                    />
-                ) : (
-                    <FaRegHeart
-                        onClick={() => dispatch(addFavorite(character))}
-                        size={30}
-                    />
-                )}
-            </div>
-        </motion.div>
-    );
+        <span className="font-bold w-full">{character.species}</span>
+      </Link>
+      <div className="flex w-full justify-between items-center">
+        <span className="font-bold">{character.location.name}</span>
+        {isFavorite ? (
+          <FaHeart
+            fill="red"
+            onClick={() => dispatch(removeFavorite(character))}
+            size={28}
+          />
+        ) : (
+          <FaRegHeart
+            onClick={() => dispatch(addFavorite(character))}
+            size={30}
+          />
+        )}
+      </div>
+    </motion.div>
+  );
 };
 
 export default CharacterCard;
