9f9be95eccaad0311268b5099cbca8d97e7e0868
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-11-01 23:13:55 -0500

    pageprops

--- src/app/character/[id]/page.tsx
+++ src/app/character/[id]/page.tsx
@@ -1,3 +1,4 @@
+// src/app/character/[id]/page.tsx
 import React from "react";
 import { Character } from "../../../../types/api/characters";
 import Image from "next/image";
@@ -7,6 +8,7 @@ import { Episode } from "../../../../types/api/episodes";
 import EpisodeCard from "@/components/episodes/EpisodeCard";
 
 const CharacterDetail = async ({ params }: { params: { id: string } }) => {
+  // Fetch character details
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_CHARACTERS_URL}/${params.id}`,
     {
@@ -19,10 +21,9 @@ const CharacterDetail = async ({ params }: { params: { id: string } }) => {
   }
 
   const character: Character = await res.json();
-  console.log(character);
 
-  const episodesUrls = character.episode;
-  const episodePromises = episodesUrls.map((url) =>
+  // Fetch episodes concurrently
+  const episodePromises = character.episode.map((url) =>
     fetch(url).then((res) => res.json())
   );
   const episodes: Episode[] = await Promise.all(episodePromises);
@@ -43,12 +44,12 @@ const CharacterDetail = async ({ params }: { params: { id: string } }) => {
           <h1 className="text-[30px] my-5 font-bold">{character.name}</h1>
           <div className="flex items-center gap-2">
             <h2 className="font-bold">Estado:</h2>
-            {character.status == "Dead" ? (
+            {character.status === "Dead" ? (
               <>
                 Muerto
                 <FaCircle fill="red" size={10} />
               </>
-            ) : character.status == "Alive" ? (
+            ) : character.status === "Alive" ? (
               <>
                 Vivo
                 <FaCircle fill="lightgreen" size={10} />
@@ -57,26 +58,23 @@ const CharacterDetail = async ({ params }: { params: { id: string } }) => {
               <>Desconocido</>
             )}
           </div>
-          <h3 className="">
+          <h3>
             <span className="font-bold">Especie:</span> {character.species}
           </h3>
-          <Link
-            className="cursor-pointer"
-            href={`locations/location/${character.id}`}
-          >
+          <Link className="cursor-pointer" href={`/locations/location/${character.id}`}>
             Origen: {character.origin.name}
           </Link>
           <p>
-            <span className="font-bold">Genero:</span> {character.gender}
+            <span className="font-bold">Género:</span> {character.gender}
           </p>
         </div>
         <div className="w-[70%]">
           <p className="text-[30px]">Ubicado en: {character.location.name}</p>
           <p className="text-[30px]">Viene de: {character.origin.name}</p>
-          <p>Episodios en los que sale el personaje:</p>
+          <p>Episodios en los que aparece el personaje:</p>
           <div className="flex flex-wrap gap-[20%] w-full lg:h-[70vh] p-20 overflow-x-hidden lg:overflow-y-scroll">
-            {episodes.map((element) => (
-              <EpisodeCard episode={element} key={element.id} />
+            {episodes.map((episode) => (
+              <EpisodeCard episode={episode} key={episode.id} />
             ))}
           </div>
         </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,30 +1,43 @@
-import Link from 'next/link'
-import React, { ReactNode } from 'react'
+import Link from "next/link";
+import React, { ReactNode } from "react";
 // import { CiHeart } from 'react-icons/ci'
-import { IoSearchOutline } from 'react-icons/io5'
+import { IoSearchOutline } from "react-icons/io5";
 
 const NavBar: React.FC<{ children: ReactNode }> = ({ children }) => {
-    return (
-        <>
-            <nav className='flex fixed z-50 top-0 w-screen justify-between items-center lg:px-10 bg-slate-600 py-3'>
-                <Link href={'/'} className='text-[#7FB226] text-[1.5rem] font-bold cursor-pointer'>
-                    RickMortyApp
-                </Link>
+  return (
+    <>
+      <nav className="flex fixed z-50 top-0 w-screen justify-between items-center lg:px-10 bg-slate-600 py-3">
+        <Link
+          href={"/"}
+          className="text-[#7FB226] text-[1.5rem] font-bold cursor-pointer"
+        >
+          RickMortyApp
+        </Link>
 
-                {/* <div className='flex gap-3 items-center cursor-pointer'>
+        {/* <div className='flex gap-3 items-center cursor-pointer'>
                     <CiHeart size={25} />
                     Favorites
                 </div> */}
-                <Link className='flex gap-3 items-center cursor-pointer' href={'/episodes'}>Episodios</Link>
-                <Link className='flex gap-3 items-center cursor-pointer' href={'/locations'}>Ubicaciones</Link>
-                <div className='relative w-[30%]'>
+        <Link
+          className="flex hover:text-green-400 gap-3 items-center cursor-pointer"
+          href={"/episodes"}
+        >
+          Episodios
+        </Link>
+        <Link
+          className="flex hover:text-green-400 gap-3 items-center cursor-pointer"
+          href={"/locations"}
+        >
+          Ubicaciones
+        </Link>
+        {/* <div className='relative w-[30%]'>
                     <input type="text" placeholder='Ingrese un personaje' className='w-full px-2 py-2 rounded-xl bg-white' />
                     <IoSearchOutline className='absolute cursor-pointer right-3 top-2' color='black' size={25} />
-                </div>
-            </nav>
-            {children}
-        </>
-    )
-}
+                </div> */}
+      </nav>
+      {children}
+    </>
+  );
+};
 
-export default NavBar
+export default NavBar;
