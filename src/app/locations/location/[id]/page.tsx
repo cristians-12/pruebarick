4d1534d891306b878852dd9bfd0e77490589c3c0
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-11-02 13:11:58 -0500

    agregar opcion de guardar en base de datos

--- src/app/locations/location/[id]/page.tsx
+++ src/app/locations/location/[id]/page.tsx
@@ -1,48 +1,47 @@
-// pages/character/[id].tsx
+'use client'
 import React, { useEffect } from "react";
 import { Character } from "../../../../../types/api/characters";
 import CharacterCard from "@/components/character/CharacterCard";
 import { Location } from "../../../../../types/api/locations";
 import { useParams } from "next/navigation";
 import { API_LOCATIONS_URL } from "../../../../../constants";
+import usePageData from "../../../../../hooks/usePageData";
+import Loader from "@/components/Loader";
 
 const LocationDetail = () => {
   const { id } = useParams();
-  const res = fetch(
-    `${process.env.NEXT_PUBLIC_API_LOCATIONS_URL}/${id}`
-  );
+  const { changeLocation, location, changeCharacters, characters } =
+    usePageData();
 
   const fetchLocation = async () => {
-    const res = await fetch(
-      `${API_LOCATIONS_URL}/${id}`
+    const res = await fetch(`${API_LOCATIONS_URL}/${id}`);
+    const location: Location = await res.json();
+    changeLocation(location);
+
+    const characterPromises = location.residents.map((url) =>
+      fetch(url).then((res) => res.json())
     );
+    const characters: Character[] = await Promise.all(characterPromises);
+    changeCharacters(characters); // Actualizamos los personajes
   };
 
-  useEffect(
-    () => {
-      fetchLocation()
-    }, []
-  )
+  useEffect(() => {
+    fetchLocation();
+  }, []);
 
-
-  const location: Location = await res.json();
-
-  const residentUrls = location.residents;
-  const residentPromises = residentUrls.map((url) =>
-    fetch(url).then((res) => res.json())
-  );
-  const residents: Character[] = await Promise.all(residentPromises);
-
-  return (
+  return location ? (
     <div className="px-5">
       <h1 className="text-[30px] font-bold">{location.name}</h1>
       <p className="my-3">Personajes relacionados al lugar:</p>
       <div className="flex flex-wrap gap-10 justify-around">
-        {residents.map((element) => (
-          <CharacterCard character={element} key={element.id} />
-        ))}
+        {characters &&
+          characters.map((element) => (
+            <CharacterCard character={element} key={element.id} />
+          ))}
       </div>
     </div>
+  ) : (
+    <Loader />
   );
 };
 
