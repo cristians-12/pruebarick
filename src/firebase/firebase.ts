016c0b75adad4b3e57b86adc7a7d6786b2f45435
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-11-04 11:19:37 -0500

    SECRETS FIREBASE

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,15 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAo9WTa9Ojtmcw36iqtrFghUtLORHbUXhI",
-  authDomain: "rickymorty-d5a3e.firebaseapp.com",
+  apiKey: `${process.env.FIREBASE_API_KEY}`,
+  authDomain: `${process.env.FIREBASE_DOMAIN_URL}`,
   projectId: "rickymorty-d5a3e",
   storageBucket: "rickymorty-d5a3e.firebasestorage.app",
   messagingSenderId: "128391058935",
@@ -17,7 +11,5 @@ const firebaseConfig = {
   measurementId: "G-9NMN7EGYXJ",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-// const analytics = getAnalytics(app);
